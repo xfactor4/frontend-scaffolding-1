24740b6e3c6fed0a7e95891582c3f2a86e1b2f0f
Jacob Smith <jacob@jacobsmith.io>
2015-09-28 16:11:15 -0400

    Update 2015-09-28T20:11:14.057Z

--- app/scripts/main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  // prepend the contents of `app/templates/application.hbs` into `body`
-  $('#container').append(JST.application());
-});
--- tests/hello-test.js
+++
@@ -1,7 +0,0 @@
-var expect = chai.expect;
-
-describe("delete me", function(){
-  it("should be deleted", function(){
-    expect(true).to.equal(true);
-  });
-});
